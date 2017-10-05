c75eaad35ba828cda3bb4317ceb0eb7b3b410d8c
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-10-05 17:08:11 +0700

    Week07P03

--- Week07/Week07/Program.cs
+++ Week07/Week07/Program.cs
@@ -12,21 +12,51 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             Student su = new Student();
-            su.Name = "Student Name";
-            su.ID = "12345678";
-            su.GPA = 2.5f;
-            Console.WriteLine("Student name : " + su.Name);
-            Console.WriteLine("Student ID   : " + su.ID);
-            Console.WriteLine("Student GPA  : " + su.GPA);
+            try
+            {
+                su.Name = "Student Name";
+                su.ID = "12345678";
+                su.GPA = 3.5f;
+                Console.WriteLine("Student name : " + su.Name);
+                Console.WriteLine("Student ID   : " + su.ID);
+                Console.WriteLine("Student GPA  : " + su.GPA);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
     class Student
     {
-        private string Name;
-        private string ID;
-        private float GPA;
-        
+        private string name;
+        private string id;
+        private float gpa;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public float GPA
+        {
+            get
+            {
+                return gpa;
+            }
+            set
+            {
+                if (value > 0.0 && value <= 4.0)
+                    gpa = value;
+                else
+                    throw (new Exception("Error!!!! invalid GPA"));
+            }
+        }
     }
 
 }
