d1c19488fb06f2708fd02c40f7bf8f4aa34a405f
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-10-05 18:28:25 +0700

    Week07P06

--- Week07/Week07/Program.cs
+++ Week07/Week07/Program.cs
@@ -7,60 +7,26 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
-    class Program
+    class Teacher
     {
-        static void Main(string[] args)
+        // constructor (for initial private/protected variables)
+        public Teacher(string name, float billingRate)
         {
-            Student su = new Student("Student Name", "12345678");
-            try
-            {
-                //su.Name = "My name";
-                su.GPA = 4.5f;
-                Console.WriteLine("Student name : " + su.Name);
-                Console.WriteLine("Student ID   : " + su.ID);
-                Console.WriteLine("Student GPA  : " + su.GPA);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            Console.ReadLine();
+            this.name = name;
+            this.billingRate = billingRate;
         }
-    }
-    class Student
-    {
+        // figure out the charge based on teacher's rate
+        public float CalculateCharge(float hours)
+        {
+            return (hours * billingRate);
+        }
+        // return the name of this type
+        public string TypeName()
+        {
+            return ("Teacher");
+        }
         private string name;
-        private string id;
-        private float gpa;
-
-        public Student(string Name, string ID)
-        {
-            this.name = Name;   // assign auto variable to the field
-            this.id = ID;
-        }
-
-        public string Name
-        {
-            get { return name; }
-        }
-        public string ID
-        {
-            get { return id; }
-        }
-        public float GPA
-        {
-            get
-            {
-                return gpa;
-            }
-            set
-            {
-                if (value > 0.0 && value <= 4.0)
-                    gpa = value;
-                else
-                    throw (new Exception("Error!!!! invalid GPA"));
-            }
-        }
+        protected float billingRate;
     }
 
 }
