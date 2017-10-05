c84750fe81aff8683c95db4cca696a4391354613
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-10-05 18:03:42 +0700

    Week07P04

--- Week07/Week07/Program.cs
+++ Week07/Week07/Program.cs
@@ -7,25 +7,60 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student su = new Student("Student Name", "12345678");
+            try
+            {
+                // su.Name = "My name";
+                su.GPA = 3.5f;
+                Console.WriteLine("Student name : " + su.Name);
+                Console.WriteLine("Student ID   : " + su.ID);
+                Console.WriteLine("Student GPA  : " + su.GPA);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
     class Student
     {
         private string name;
         private string id;
         private float gpa;
+
+        public Student(string Name, string ID)
+        {
+            this.name = Name;   // assign auto variable to the field
+            this.id = ID;
+        }
+
         public string Name
         {
             get { return name; }
-            set { name = value; }
         }
         public string ID
         {
             get { return id; }
-            set { id = value; }
         }
         public float GPA
         {
-            get { return gpa; }
-            set { gpa = value; }
+            get
+            {
+                return gpa;
+            }
+            set
+            {
+                if (value > 0.0 && value <= 4.0)
+                    gpa = value;
+                else
+                    throw (new Exception("Error!!!! invalid GPA"));
+            }
         }
     }
+
 }
